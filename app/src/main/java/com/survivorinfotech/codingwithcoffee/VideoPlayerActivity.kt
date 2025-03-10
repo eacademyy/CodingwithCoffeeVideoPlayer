2dada67402ef494e5eaa29e391b1835d5fafa15d
eacademyy <eacademyy@gmail.com>
2025-03-10 22:45:15 +0530

    Third Commit

--- app/src/main/java/com/survivorinfotech/codingwithcoffee/VideoPlayerActivity.kt
+++ app/src/main/java/com/survivorinfotech/codingwithcoffee/VideoPlayerActivity.kt
@@ -17,13 +17,13 @@ class VideoPlayerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
+        enableEdgeToEdge()
         setContentView(R.layout.activity_video_player)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         pvVideo = findViewById(R.id.pvVideo)
 
